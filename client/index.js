677c8b24239e6ed1b0a8515b55eaaa30d82b6745
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-19 15:50:03 +0200

    Update content and fix bugs for index.js file in client folder

--- client/index.js
+++ client/index.js
@@ -1,3 +1,11 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
